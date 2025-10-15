600d6ca6de534b7648a92ffd55b6b38b28484984
Barish1234 <barish.chakraborty@niser.ac.in>
2025-10-15 14:01:11 +0530

    Add files via upload

---
+++ .github/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Comp',
+    version='1.0',
+    packages=find_packages(),
+    description='',
+    author='Barish Chakraborty',
+    author_email='barish.chakraborty@niser.ac.in',
+    url='https://github.com/Barish1234/Comp',
+)
+
